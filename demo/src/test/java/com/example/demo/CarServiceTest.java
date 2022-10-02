dee63c1812a16a14f2e5916f30edc7204313a5c2
Bianka Modrovičová <bibisvk@gmail.com>
2022-10-02 12:33:55 +0200

    add CarServiceTest

---
+++ demo/src/test/java/com/example/demo/CarServiceTest.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import com.example.demo.cars.dal.CarEntity;
+import com.example.demo.cars.dal.CarRepository;
+import com.example.demo.cars.service.CarDto;
+import com.example.demo.cars.service.CarService;
+import com.example.demo.exceptions.WrongNumberOfDoors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CarServiceTest {
+
+    @InjectMocks
+    private CarService carService;
+
+    @Mock
+    private CarRepository carRepository;
+
+    @Test
+    public void setCarServiceSaveValid() throws WrongNumberOfDoors {
+        int generatedId = 0;
+
+        CarEntity fakeEntity = new CarEntity().setId(generatedId)
+                .setPrice("medium")
+                .setDoors(5)
+                .setEKEndDate(LocalDate.parse("2022-06-06"))
+                .setPersons(3)
+                .setLug_boot("small")
+                .setSafety("medium")
+                .setPZPEndDate(LocalDate.parse("2022-06-06"))
+                .setTKEndDate(LocalDate.parse("2022-06-06"));
+
+        when(carRepository.save(any(CarEntity.class)))
+                .thenReturn(fakeEntity);
+
+        CarDto carToSave = new CarDto().setPrice("medium")
+                .setDoors(5)
+                .setEKEndDate(LocalDate.parse("2022-06-06"))
+                .setPersons(3)
+                .setLug_boot("small")
+                .setSafety("medium")
+                .setPZPEndDate(LocalDate.parse("2022-06-06"))
+                .setTKEndDate(LocalDate.parse("2022-06-06"));
+
+        int id = carService.createCar(carToSave);
+
+        assertEquals(generatedId, id);
+        verify(carRepository,times(1)).save(any());
+    }
+}
